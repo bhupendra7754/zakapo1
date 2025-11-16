866cde56153684a4a27834dce4fee7aeef8b5acd
Ankit <yadava9042@gmail.com>
2025-11-16 18:11:29 +0530

    testimonial update

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -23,99 +23,98 @@ const LandingPage = () => {
         </button>
       </div>
 
-      <section>
-        {/* hero section */}
-        <div className="absolute w-[1056px] h-[437px] top-[106px] left-[66px] border">
-          <img
-            src="/images/heroimages/Group.png"
-            className="absolute w-[63.75px] h-[83.45px] top-0 left-[965px] rotate-0 opacity-100"
-          />
-          {/* left-[1011px] */}
-
-          <h1 className="font-['Abhaya_Libre'] font-bold text-[80px] leading-[95%] tracking-[0px] text-center mt-[36px] text-[#000000]">
-            The Future of{" "}
-            <span className="font-['Abhaya_Libre'] font-bold text-[80px] leading-[95%] tracking-[0px] text-center text-[#004F97]">
-              E-commerce
-            </span>{" "}
-            Intelligence is Coming
-          </h1>
-          <div class="absolute w-[585px] h-[90px] top-[225px] ml-[236px]">
-            <p class="font-poppins font-normal text-[20px] leading-[100%] tracking-[0]  text-center text-[#000000]  ">
-              AI that helps D2C brands predict demand, optimize stock &
-              <span class="block h-[0.5rem]"></span>
-              boost profits. Get full transparency into
-              <span class="block h-[0.5rem]"></span>
-              performance, cost, and every action.
-            </p>
-          </div>
-        </div>
-
-        {/* =================will fix effects of button============ */}
-        {/* effect of left button */}
-        <div className="absolute w-[44px] h-[48px] top-[457px] left-[444px]">
-          <img
-            src="/images/heroimages/Vector.png"
-            alt=""
-            className="absolute w-[3.88px] h-[15.08px] top-0 left-[39.05px] rotate-[13.26deg] opacity-100"
-          />
-          <img
-            src="/images/heroimages/Vector.png"
-            alt=""
-            className="absolute w-[24.3px] h-[23.21px] top-[11.2px] left-[5.3px] rotate-[13.26deg] opacity-100"
-          />
-          <img
-            src="/images/heroimages/Vector.png"
-            alt=""
-            className="absolute w-[15.91px] h-[5.53px] top-[47.78px] left-[9.74px] rotate-[13.26deg] opacity-100"
-          />
-        </div>
-
-        {/* join the wishlist button */}
-        <div className="absolute w-[213px] h-[66px] top-[477px] left-[487px] rounded-[50px] bg-[#0076E0] items-center justify-center cursor-pointer flex">
-          <button className="font-[Poppins] font-bold text-[20px] leading-[100%] tracking-[0] text-[#ffff] w-[176.46px] h-[30px]">
-            Join the Wishlist
-          </button>
-        </div>
-
-        {/* right side effect */}
-        <div className="absolute w-[44.00000095642667px] h-[48.00000104337455px] top-[510px] left-[689px] rotate-[-166.74deg] opacity-100">
-          <img
-            src="/images/heroimages/Vector.png"
-            alt=""
-            className="absolute w-[3.88px] h-[15.08px] top-0 left-[39.05px] rotate-[13.26deg] opacity-100"
-          />
-          <img
-            src="/images/heroimages/Vector.png"
-            alt=""
-            className="absolute w-[24.3px] h-[23.21px] top-[11.2px] left-[5.3px] rotate-[13.26deg] opacity-100"
-          />
-          <img
-            src="/images/heroimages/Vector.png"
-            alt=""
-            className="absolute w-[15.91px] h-[5.53px] top-[47.78px] left-[9.74px] rotate-[13.26deg] opacity-100"
-          />
-        </div>
-
-        {/* shping cart image */}
-
-        {/* =======abhi fix krna hai ise ========= */}
+      {/* hero section */}
+      <div className="absolute w-[1056px] h-[437px] top-[106px] left-[66px] ">
         <img
-          src="/images/heroimages/Shopping Cart.png"
-          alt=""
-          className="absolute z-10 top-[255px] left-[51px] w-[120px] h-[120px] object-contain"
+          src="/images/heroimages/Group.png"
+          className="absolute w-[63.75px] h-[83.45px] top-0 left-[965px] rotate-0 opacity-100"
         />
-        <div className="w-[167px] h-[131px] top-[269px] absolute border left-[5px]">
-          <img
-            src="/images/heroimages/Ellipse 5.png"
-            alt=""
-            className="rotate-[-41.06deg] opacity-100"
-          />
+        {/* left-[1011px] */}
+
+        <h1 className="font-['Abhaya_Libre'] font-bold text-[80px] leading-[95%] tracking-[0px] text-center mt-[36px] text-[#000000]">
+          The Future of{" "}
+          <span className="font-['Abhaya_Libre'] font-bold text-[80px] leading-[95%] tracking-[0px] text-center text-[#004F97]">
+            E-commerce
+          </span>{" "}
+          Intelligence is Coming
+        </h1>
+        <div class="absolute w-[585px] h-[90px] top-[225px] ml-[236px]">
+          <p class="font-poppins font-normal text-[20px] leading-[100%] tracking-[0]  text-center text-[#000000]  ">
+            AI that helps D2C brands predict demand, optimize stock &
+            <span class="block h-[0.4rem]"></span>
+            boost profits. Get full transparency into
+            <span class="block h-[0.4rem]"></span>
+            performance, cost, and every action.
+          </p>
         </div>
-        <div>
-          <img
-            src="/images/heroimages/Box.png"
-            alt=""
-            className="
+      </div>
+
+      {/* =================will fix effects of button============ */}
+      {/* effect of left button */}
+      <div className="absolute w-[44px] h-[48px] top-[457px] left-[444px]">
+        <img
+          src="/images/heroimages/Vector.png"
+          alt=""
+          className="absolute w-[3.88px] h-[15.08px] top-0 left-[39.05px] rotate-[13.26deg] opacity-100"
+        />
+        <img
+          src="/images/heroimages/Vector.png"
+          alt=""
+          className="absolute w-[24.3px] h-[23.21px] top-[11.2px] left-[5.3px] rotate-[13.26deg] opacity-100"
+        />
+        <img
+          src="/images/heroimages/Vector.png"
+          alt=""
+          className="absolute w-[15.91px] h-[5.53px] top-[47.78px] left-[9.74px] rotate-[13.26deg] opacity-100"
+        />
+      </div>
+
+      {/* join the wishlist button */}
+      <div className="absolute w-[213px] h-[66px] top-[477px] left-[487px] rounded-[50px] bg-[#0076E0] items-center justify-center cursor-pointer flex">
+        <button className="font-[Poppins] font-bold text-[20px] leading-[100%] tracking-[0] text-[#ffff] w-[176.46px] h-[30px]">
+          Join the Wishlist
+        </button>
+      </div>
+
+      {/* right side effect */}
+      <div className="absolute w-[44.00000095642667px] h-[48.00000104337455px] top-[510px] left-[689px] rotate-[-166.74deg] opacity-100">
+        <img
+          src="/images/heroimages/Vector.png"
+          alt=""
+          className="absolute w-[3.88px] h-[15.08px] top-0 left-[39.05px] rotate-[13.26deg] opacity-100"
+        />
+        <img
+          src="/images/heroimages/Vector.png"
+          alt=""
+          className="absolute w-[24.3px] h-[23.21px] top-[11.2px] left-[5.3px] rotate-[13.26deg] opacity-100"
+        />
+        <img
+          src="/images/heroimages/Vector.png"
+          alt=""
+          className="absolute w-[15.91px] h-[5.53px] top-[47.78px] left-[9.74px] rotate-[13.26deg] opacity-100"
+        />
+      </div>
+
+      {/* shping cart image */}
+
+      {/* =======abhi fix krna hai ise ========= */}
+      <img
+        src="/images/heroimages/Shopping Cart.png"
+        alt=""
+        className="absolute z-10 top-[255px] left-[51px] w-[120px] h-[120px] object-contain"
+      />
+      <div className="w-[167px] h-[131px] top-[269px] absolute border left-[5px]">
+        <img
+          src="/images/heroimages/Ellipse 5.png"
+          alt=""
+          className="rotate-[-41.06deg] opacity-100"
+        />
+      </div>
+      <div>
+        <img
+          src="/images/heroimages/Box.png"
+          alt=""
+          className="
     w-[120.00000358982918px]
     h-[120.00000358982918px]
     top-[424px]
@@ -124,22 +123,22 @@ const LandingPage = () => {
     opacity-100
     absolute
     z-10"
-          />
+        />
 
-          <img
-            src="/images/heroimages/Ellipse 3.png"
-            alt=""
-            className="w-[148px] absolute h-[116px] top-[444px] left-[140.85px] rotate-[-41.06deg]
+        <img
+          src="/images/heroimages/Ellipse 3.png"
+          alt=""
+          className="w-[148px] absolute h-[116px] top-[444px] left-[140.85px] rotate-[-41.06deg]
     opacity-100 rounded-full "
-          />
-        </div>
+        />
+      </div>
 
-        {/* right side images */}
-        <div>
-          <img
-            src="/images/heroimages/Group 8133.png"
-            alt=""
-            className="
+      {/* right side images */}
+      <div>
+        <img
+          src="/images/heroimages/Group 8133.png"
+          alt=""
+          className="
     w-[188.40382385253906px]
     h-[190.75340270996094px]
     top-[265px]
@@ -148,11 +147,11 @@ const LandingPage = () => {
     opacity-100
     absolute
   "
-          />
-          <img
-            src="/images/heroimages/Group 8134.png"
-            alt=""
-            className="
+        />
+        <img
+          src="/images/heroimages/Group 8134.png"
+          alt=""
+          className="
     w-[229.86587524414062px]
     h-[236.40570068359375px]
     top-[416px]
@@ -161,9 +160,256 @@ const LandingPage = () => {
     opacity-100
     absolute
   "
-          />
-        </div>
-      </section>
+        />
+      </div>
+
+      {/* companies logo section */}
+      
+      
+      <div
+  className="
+    w-[825px]
+    h-[140px]
+    top-[628px]
+    left-[185px]
+    opacity-100
+    absolute   
+  "
+>
+  <div className="w-[760px]
+    h-[48px]
+    top-[628px]
+    left-[214px]">
+<h1
+  className="
+    font-poppins
+    font-medium
+    text-[32px]
+    leading-[100%]
+    tracking-[0px]
+    text-center
+  "
+>
+  Integrate seamlessly for Top{" "}
+  <span
+    className="
+      font-poppins
+      font-medium
+      text-[32px]
+      leading-[100%]
+      tracking-[0px]
+      text-center
+      text-[#004F97]
+    "
+  >
+    D2C
+  </span>{" "}
+  Marketplaces
+</h1>
+
+
+
+    </div>
+
+
+    <div className="
+    w-[825px]
+    h-[40px]
+    
+    
+    bottom-0 absolute
+    
+    
+    
+
+    
+      
+  ">
+    <img src="/images/heroimages/companiesLogo/download (10) 1.png" alt=" companiesLogo"
+    className="
+    w-[103px]
+   
+    
+    
+    rotate-[0deg]
+    opacity-100
+    absolute
+  " />
+   <img src="/images/heroimages/companiesLogo/Group 5.png" alt=" companiesLogo"
+    className="
+    w-[88px]
+    
+    ml-[143px]
+    
+    rotate-[0deg]
+    opacity-100
+    absolute
+  " />
+   <img src="/images/heroimages/companiesLogo/download (7) 1.png" alt=" companiesLogo"
+    className="
+    w-[157px]
+    
+    
+    rotate-[0deg]
+    opacity-100
+    absolute
+    left-[271px]
+  " />
+   <img src="/images/heroimages/companiesLogo/Group 8104.png" alt=" companiesLogo"
+    className="
+    w-[162px]
+    
+    left-[468px]
+    
+    rotate-[0deg]
+    opacity-100
+    absolute
+  " />
+  <img src=" /images/heroimages/companiesLogo/download (9) 1.png" alt=" companiesLogo"
+    className="
+    w-[155px]
+   
+    left-[670px]
+    
+    rotate-[0deg]
+    opacity-100
+    absolute
+  " />
+  
+  
+  </div>
+ 
+</div>
+
+{/* ===== */}
+<div
+  className="
+    w-[1087px]
+    h-[184px]
+    top-[815px]
+    left-[51px]
+    
+    opacity-100
+    absolute border
+  "
+>
+  <img src="/images/heroimages/Quote Left.png" alt=""
+  className="  w-[41px]
+    h-[41px] ml-[18px]
+    top-0
+   
+    opacity-100 absolute border" /> 
+    
+
+    <div   className="
+    w-[1023px]
+    h-[96px]
+    mt-[6px]
+    
+    opacity-100
+     border  flex absolute  
+  ">
+    <h3
+  className="
+    font-poppins 
+    font-medium
+    text-[32px]
+    leading-[100%]
+    tracking-[0px]
+    text-center top-[815px] left-0
+  "
+>
+  You don’t need 1000 people to scale like a giant. You just need{" "}
+  <span
+    className="
+      font-poppins
+      font-medium
+      text-[32px]
+      leading-[100%]
+      tracking-[0px]
+      
+      text-[#004F97]
+    "
+  >
+    Zakapo
+  </span>
+  . The only platform built for Indian e-commerce growth.
+</h3>
+
+  </div>
+  <img src="/images/heroimages/Quote Left.png" alt=""className="w-[40px] h-[40px] absolute mt-[41px] left-[970px] rotate-[-180deg] opacity-100 "
+ />
+
+  
+</div>
+
+{/* effect */}
+{/* ye sahi haiiii */}
+
+<div className="w-[745px] h-[115px] top-[884px] left-[190px] absolute"
+>
+  <img src="/images/heroimages/Group copy.png" alt=""
+  className="w-[742.2087885015982px] h-[68.08042951198814px] left-[-0.01px] rotate-[-3.63deg] opacity-100 absolute"
+ />
+</div>
+
+
+{/* ========== */}
+
+
+<section className="w-[1075.4931640625px] h-[742px] top-[1034px] left-[51px] opacity-100 absolute border">
+
+  <div
+    className="
+      border
+      w-[867px]
+      h-[105.7px]
+      mx-auto
+      flex
+      justify-center
+      items-center
+    "
+  >
+    <p
+  className="
+    font-abhaya-libre
+    font-bold
+    text-[60px]
+    leading-[95%]
+    tracking-[0px]
+    text-[#000000]
+    text-center
+   
+  "
+>
+  How{" "}
+  <span
+    className="
+      font-abhaya-libre
+      font-bold
+      text-[60px]
+      leading-[95%]
+      tracking-[0px]
+      
+      text-center
+      text-[#004F97]
+    "
+  >
+    Zakapo
+   
+  </span>
+   {" "}
+  
+  turns your store data  into strategic decisions
+</p>
+
+  </div>
+
+</section>
+
+      
+
+    
     </section>
   );
 };
